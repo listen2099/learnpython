b010b4abfe675a63a3c695d05603cf5612b52efd
listen2099 <listen2099@gmail.com>
2020-11-21 15:58:19 +0800

    1121

--- 1.processes_and_threads/t2.py
+++ 1.processes_and_threads/t2.py
@@ -1,11 +1,16 @@
 import threading
+import logging
 
+FORMAT = '%(asctime)-15s\t [%(threadName)s,%(thread)8d] %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 def worker():
     for x in range(100):
-        print("{} is running!".format(threading.current_thread().name))
-
+        # print("{} is running!\n".format(threading.current_thread().name), end='')  # 线程不安全
+        logging.info("{} is running!".format(threading.current_thread().name))  # 线程安全
 
 for i in range(5):
     name = "worker-{}".format(i)
     threading.Thread(target=worker).start()
+
+
